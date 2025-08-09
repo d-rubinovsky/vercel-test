83e42219c289183d8c2e5856b66f4497461fd0e7
Dmitry Rubinovsky <dmitry.rubinovsky@gmail.com>
2025-08-10 01:28:10 +0300

    Uncomment security

--- apps/app/env.ts
+++ apps/app/env.ts
@@ -6,7 +6,7 @@ import { keys as auth } from '@repo/auth/keys';
 import { keys as core } from '@repo/next-config/keys';
 // import { keys as notifications } from '@repo/notifications/keys';
 // import { keys as observability } from '@repo/observability/keys';
-// import { keys as security } from '@repo/security/keys';
+import { keys as security } from '@repo/security/keys';
 // import { keys as webhooks } from '@repo/webhooks/keys';
 import { createEnv } from '@t3-oss/env-nextjs';
 
@@ -20,7 +20,7 @@ export const env = createEnv({
     // flags(),
     // notifications(),
     // observability(),
-    // security(),
+    security(),
     // webhooks(),
   ],
   server: {},
